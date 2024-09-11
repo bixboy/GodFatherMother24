2ad1bc5d9d6190966166b2ff5d5212512755e8a9
bixboy <pierrelamare62@gmail.com>
2024-09-11 14:30:33 +0200

    Implement Font & Add Night/Day & Add MainMenu

---
+++ Assets/ScriptsPierre/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+    private void PlayGame()
+    {
+        SceneManager.LoadScene("EmaScene");
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/ScriptsPierre/NightDay.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NightDay : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 10f;
+    [SerializeField] private float _radius = 10f;
+    private Animator _animator;
+
+    [SerializeField] private Transform _sun;
+    [SerializeField] private Transform _moon;
+    [SerializeField] private GameObject _centerPoint;
+    private Vector3 _centerPos;
+    private float _rotationSpeedTemp;
+
+    public void ChangeDay() => StartChangeDay();
+
+    void Start()
+    {
+        _rotationSpeedTemp = _rotationSpeed;
+        _rotationSpeed = 0;
+        _animator = GetComponent<Animator>();
+
+        _centerPos = _centerPoint.GetComponent<RectTransform>().position;
+        _sun.position = _centerPos + new Vector3(0f, _radius, 0f);
+        _moon.position = _centerPos + new Vector3(0f, -_radius, 0f);
+    }
+
+    private void StartChangeDay()
+    {
+        _animator.SetBool("EndChangeDay", false);
+        _animator.SetTrigger("StartChangeDay");
+        _rotationSpeed = _rotationSpeedTemp;
+
+        StartCoroutine(RotateAroundCenter(_sun, 1, 180f));
+        StartCoroutine(RotateAroundCenter(_moon, 1, 180f));
+    }
+
+    private void ChangeRoom()
+    {
+
+        EndChangeDay();
+    }
+
+    private void EndChangeDay()
+    {
+        _animator.SetBool("EndChangeDay", false);
+    }
+
+    private IEnumerator RotateAroundCenter(Transform obj, int direction, float targetRotation)
+    {
+        float currentRotation = 0f;
+
+        while (currentRotation < targetRotation)
+        {
+            float rotationStep = _rotationSpeed * Time.deltaTime;
+
+            if (currentRotation + rotationStep > targetRotation)
+            {
+                rotationStep = targetRotation - currentRotation;
+            }
+
+            obj.RotateAround(_centerPos, Vector3.forward, direction * rotationStep);
+            obj.rotation = Quaternion.identity;
+
+            currentRotation += rotationStep;
+
+            yield return null;
+        }
+        ChangeRoom();
+    }
+}
